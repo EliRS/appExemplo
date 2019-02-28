cfe14c48e71df6f817f1aa11255018bb9570bd90
EliRS <eliasrodriguesdossantos100@gmail.com>
2019-03-01 02:38:44 +0900

    aplicando funções

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -5,15 +5,44 @@
   valor += "7";
   $("#display").val(valor);
 });*/
+var pegar;
 
 $(document).on('click','.botao',function(){
   var valor;
   valor = $("#display").val();
   valor += $(this).val();
   $("#display").val(valor);
+  if($("#display").val().length == 1){
+    $("#vir").attr("disabled", false);
+  }
 });
 
 $(document).on('click','#ce',function(){
   $("#display").val("");
+  pegar = 0;
 });
+
+$(document).on('click','#vir',function(){
+  var valor;
+  valor = $("#display").val();
+  valor += $(this).val();
+  $("#display").val(valor);
+  $("#vir").attr("disabled", true);
+});
+
+
+$(document).on('click','#plus',function(){
+  var valor;
+  if($("#display").val().length == 1){
+    $("#vir").attr("disabled", false);
+  }
+  valor = parseFloat($("#display").val());
+  $("#display").val("");
+  if(pegar == null){
+    pegar = valor;
+  }else{
+    pegar = pegar + valor;
+  }
+});
+
 
