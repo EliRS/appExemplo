cffe08bcb5ed2c16d31fc95f58890d8d85706b20
EliRS <eliasrodriguesdossantos100@gmail.com>
2019-03-01 23:09:59 +0900

    Finalização do projeto

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -6,7 +6,9 @@
   $("#display").val(valor);
 });*/
 var pegar;
+var operacao = "";
 
+// botoes 1 ao 9
 $(document).on('click','.botao',function(){
   var valor;
   valor = $("#display").val();
@@ -17,11 +19,14 @@ $(document).on('click','.botao',function(){
   }
 });
 
+//btn apagar
 $(document).on('click','#ce',function(){
   $("#display").val("");
+  operacao = "";
   pegar = 0;
 });
 
+//btn virgula
 $(document).on('click','#vir',function(){
   var valor;
   valor = $("#display").val();
@@ -31,18 +36,49 @@ $(document).on('click','#vir',function(){
 });
 
 
-$(document).on('click','#plus',function(){
+// btn +
+$(document).on('click','#operadores',function(){
   var valor;
   if($("#display").val().length == 1){
     $("#vir").attr("disabled", false);
   }
   valor = parseFloat($("#display").val());
   $("#display").val("");
-  if(pegar == null){
-    pegar = valor;
-  }else{
+  if(operacao == "+"){
     pegar = pegar + valor;
+  }else if(operacao == "-"){
+    pegar = pegar - valor;
+  }else if(operacao == "*"){
+    pegar = pegar * valor;
+  }else if(operacao == "/"){
+    pegar = pegar / valor;
+  }else{
+    pegar = valor
   }
+
+  // verificar qual é o operador
+  
+    operacao = $(this).val();
+  
+  
 });
+
+// btn igual
+$(document).on('click','#igual',function(){
+  valor = parseFloat($("#display").val());
+  if(operacao == "+"){
+    pegar = pegar + valor;
+  }else if(operacao == "-"){
+    pegar = pegar - valor;
+  }else if(operacao == "*"){
+    pegar = pegar * valor;
+  }else{
+    pegar = pegar / valor;
+  }
+  $("#display").val(pegar);
+  operacao = "";
+  pegar = 0;
+});
+
 
 
