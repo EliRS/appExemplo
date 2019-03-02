c0cbf841839675f08821d18cd9147247354121a2
EliRS <eliasrodriguesdossantos100@gmail.com>
2019-03-02 11:11:01 +0900

    Corrigindo erros

--- www/components/acoes.js
+++ www/components/acoes.js
@@ -5,24 +5,22 @@
   valor += "7";
   $("#display").val(valor);
 });*/
-var pegar;
-var operacao = "";
-var texto = "";
-var texto1 = "";
-
+var pegar, operacao = "", texto = "", texto1 = "", virgula = "";
 // botoes 1 ao 9
 $(document).on('click','.botao',function(){
   var valor;
   valor = $("#display").val();
   valor += $(this).val();
   $("#display").val(valor);
-  if($("#display").val().length == 1){
+  if($("#display").val().length == 1 || virgula == "a"){
     $("#vir").attr("disabled", false);
+    virgula = null;
   }
 });
 
 //btn apagar
 $(document).on('click','#ce',function(){
+  $("#vir").attr("disabled", true);
   $("#display").val("");
   operacao = "";
   pegar = null;
@@ -37,14 +35,10 @@ $(document).on('click','#vir',function(){
   $("#display").val(valor);
   $("#vir").attr("disabled", true);
 });
-
-
-// btn +
+// operadores
 $(document).on('click','#operadores',function(){
   var valor;
-  if($("#display").val().length == 1){
-    $("#vir").attr("disabled", false);
-  }
+  $("#vir").attr("disabled", true);
   if(pegar == null){
     valor = parseFloat($("#display").val());
     texto = $("#display").val() + $(this).val();
@@ -54,26 +48,21 @@ $(document).on('click','#operadores',function(){
     valor = parseFloat($("#display").val().slice(texto.length,texto1.length));
     texto = $("#display").val() + $(this).val();
     $("#display").val(texto);
-  }
-  
-  
+  } 
   if(operacao == "+"){
-    pegar = pegar + valor;
+    pegar += valor;
   }else if(operacao == "-"){
-    pegar = pegar - valor;
+    pegar -= valor;
   }else if(operacao == "*"){
-    pegar = pegar * valor;
+    pegar *= valor;
   }else if(operacao == "/"){
-    pegar = pegar / valor;
+    pegar /= valor;
   }else{
     pegar = valor;
   }
-
   // verificar qual é o operador
-  
-    operacao = $(this).val();
-  
-  
+  operacao = $(this).val();
+  virgula = "a";
 });
 
 // btn igual
@@ -81,19 +70,16 @@ $(document).on('click','#igual',function(){
   texto1 = $("#display").val();
   valor = parseFloat($("#display").val().slice(texto.length,texto1.length));
   if(operacao == "+"){
-    pegar = pegar + valor;
+    pegar += valor;
   }else if(operacao == "-"){
-    pegar = pegar - valor;
+    pegar -= valor;
   }else if(operacao == "*"){
-    pegar = pegar * valor;
+    pegar *= valor;
   }else{
-    pegar = pegar / valor;
+    pegar /= valor;
   }
   $("#display").val(pegar);
   operacao = "";
   pegar = null;
   texto = "";
 });
-
-
-
